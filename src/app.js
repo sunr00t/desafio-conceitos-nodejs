c2121aa29815cfcce87b9244a9256eef60ed735f
Bruno Nascimento <sunr00t@gmail.com>
2020-08-15 19:26:06 -0300

    desafio com test passsed

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
-
+const { uuid, isUuid } = require("uuidv4");
 const app = express();
 
 app.use(express.json());
@@ -10,24 +8,71 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
-});
+/** Midlewares
+ * validateRepository() 
+ * Checa se a rota tem um isUuid valido, caso não retornar status 400
+ * */
+
+function validateRepository (request, response, next) {
+    const { id } = request.params;
+    if(!isUuid(id)){
+      return response.status(400).json({error: 'Invalid repository id'})
+    }
+    return next();
+}
+
+// Aplica o middleware nas rotas que pedir query-params repositories/:id 
+app.use('/repositories/:id', validateRepository)
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+  const repository = { id: uuid(), title, url, techs, likes: 0 }
+  repositories.push(repository)
+  return response.json(repository)
+
 });
 
+app.get("/repositories", (request, response) => {
+  return response.json(repositories);
+});
+
+
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found ' })
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositoryIndex].likes
+  };
+
+  repositories[repositoryIndex] = repository
+  return response.json(repositories[repositoryIndex])
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found ' })
+  }
+  repositories.splice(repositoryIndex, 1);
+  return response.status(204).send()
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+  repositories[repositoryIndex].likes++
+  return response.json(repositories[repositoryIndex])
 });
 
 module.exports = app;
